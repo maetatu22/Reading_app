de9c6506ef2ece086860f32dce16cd5079b29f36
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-08 02:03:41 +0900

    読書メモ一覧表示ページ作成

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
+app.use(express.static('public'));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'Reading_app'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) => {
+      console.log(results);
+      res.render('top.ejs');
+    }
+  );
+});
+
+app.get('/top' , (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index.ejs');
 });
 
 app.listen(3000);
